1e1367dbdc718266bc3fee126cc9a7bb0af82022
victora <valad@gatech.edu>
2018-12-03 11:32:40 -0500

    found this code just this morning

--- src/graspy.py
+++ src/graspy.py
@@ -1,153 +1,175 @@
 #!/usr/bin/env python
 
-# Copyright (c) 2015, Fetch Robotics Inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#     * Neither the name of the Fetch Robotics Inc. nor the names of its
-#       contributors may be used to endorse or promote products derived from
-#       this software without specific prior written permission.
-# 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL FETCH ROBOTICS INC. BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-# Author: Michael Ferguson
-
-import copy
-import actionlib
+import sys
 import rospy
+import copy, math
+#import moveit_commander
 
-from math import sin, cos
-from moveit_python import (MoveGroupInterface,
-                           PlanningSceneInterface,
-                           PickPlaceInterface)
-from moveit_python.geometry import rotate_pose_msg_by_euler_angles
-from moveit_msgs.msg import Grasp, CollisionObject
-import tf
+from moveit_python import RobotCommander, MoveGroupCommander
+from moveit_python import PlanningSceneInterface, roscpp_initialize, roscpp_shutdown
+from geometry_msgs.msg import PoseStamped
+from moveit_msgs.msg import Grasp, GripperTranslation, PlaceLocation, MoveItErrorCodes
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+import random
 
-def openGripper():
+ROBOT_NAME = "fetch"
 
-  #// BEGIN_SUB_TUTORIAL open_gripper
-  #/* Add both finger joints of panda robot. */
-  JointTrajectory.joint_names.resize(2);
-  JointTrajectory.joint_names[0] = "panda_finger_joint1";
-  JointTrajectory.joint_names[1] = "panda_finger_joint2";
+if ROBOT_NAME == "fetch":
+    GROUP_NAME_ARM = 'arm_with_torso'
+    GROUP_NAME_GRIPPER = 'r_gripper'
 
-  #/* Set them as open, wide enough for the object to fit. */
-  JointTrajectory.points.resize(1);
-  JointTrajectory.points[0].positions.resize(2);
-  JointTrajectory.points[0].positions[0] = 0.04;
-  JointTrajectory.points[0].positions[1] = 0.04;
-  #JointTrajectory.points[0].time_from_start = ros::Duration(0.5);
-  #// END_SUB_TUTORIAL
+    GRIPPER_FRAME = 'r_gripper_finger_link'
 
+    FIXED_FRAME = 'base_link'
 
-def closedGripper():
-  #// BEGIN_SUB_TUTORIAL closed_gripper
-  #/* Add both finger joints of panda robot. */
-  JointTrajectory.joint_names.resize(2);
-  JointTrajectory.joint_names[0] = "panda_finger_joint1";
-  JointTrajectory.joint_names[1] = "panda_finger_joint2";
+    GRIPPER_CLOSED = 0.3
+    GRIPPER_OPEN = 0.0
 
-  #/* Set them as closed. */
-  JointTrajectory.points.resize(1);
-  JointTrajectory.points[0].positions.resize(2);
-  JointTrajectory.points[0].positions[0] = 0.00;
-  JointTrajectory.points[0].positions[1] = 0.00;
-  #JointTrajectory.points[0].time_from_start = ros::Duration(0.5);
-  #// END_SUB_TUTORIAL
-
-def addCollisionObjects(planning_scene_interface):
-    CollisionObject[0].id = "table"
-    CollisionObject[0].header.frame_id = "base_link"
-    CollisionObject[0].primitives[0].dimensions[0] = 0.2
-    CollisionObject[0].primitives[0].dimensions[1] = 0.4
-    CollisionObject[0].primitives[0].dimensions[2] = 0.4
-    #Define the pose of the table. 
-    CollisionObject[0].primitive_poses[0].position.x = 0.5
-    CollisionObject[0].primitive_poses[0].position.y = 0
-    CollisionObject[0].primitive_poses[0].position.z = 0.2
-
-    Grasp.grasp_pose.header.frame_id = "base_link"
-    orientation = tf.Quaternion()
-    orientation.setRPY(-M_PI / 2, -M_PI / 4, -M_PI / 2)
-    Grasp.grasp_pose.pose.orientation = tf.toMsg(orientation)
-    Grasp.grasp_pose.pose.position.x = 0.415
-    Grasp.grasp_pose.pose.position.y = 0
-    Grasp.grasp_pose.pose.position.z = 0.5
-
-    CollisionObject[0].operation = CollisionObject[0].ADD
-
-    #add object
-    CollisionObject[1].id = "object"
-    CollisionObject[1].header.frame_id = "base_link"
-    CollisionObject[1].primitives[0].type = CollisionObject[1].primitives[0].BOX;
-    CollisionObject[1].primitives[0].dimensions[0] = 0.02
-    CollisionObject[1].primitives[0].dimensions[1] = 0.02
-    CollisionObject[1].primitives[0].dimensions[2] = 0.2
-
-    Grasp.grasp_pose.header.frame_id = "base_link"
-    #tf2::Quaternion orientation
-    #orientation.setRPY(-M_PI / 2, -M_PI / 4, -M_PI / 2)
-    Grasp.grasp_pose.pose.orientation = tf.toMsg(orientation)
-    Grasp.grasp_pose.pose.position.x = 0.5
-    Grasp.grasp_pose.pose.position.y = 0
-    Grasp.grasp_pose.pose.position.z = 0.5
-
-    CollisionObject[0].operation = CollisionObject[0].ADD
-
-    planning_scene_interface.applyCollisionObjects(CollisionObject);
+    GRIPPER_JOINT_NAMES = ['r_gripper_finger_joint']
     
+    GRIPPER_EFFORT = [1.0]
 
-def pick(move_group):
+class TestPick():
+    def __init__(self):
+
+        roscpp_initialize(sys.argv)
+        rospy.init_node('moveit_py_demo', anonymous=True)
+       
+        scene = PlanningSceneInterface()
+        robot = RobotCommander()
+        
+        right_arm = MoveGroupCommander(GROUP_NAME_ARM)
+        right_gripper = MoveGroupCommander(GROUP_NAME_GRIPPER)
+        
+        eef = right_arm.get_end_effector_link()
+        
+        rospy.sleep(2)
+        
+        scene.remove_attached_object(GRIPPER_FRAME, "part")
+
     
-    #setting pregrasp approach
-    #Defined with respect to frame_id */
-    Grasp.pre_grasp_approach.direction.header.frame_id = "base_link";
-    #/* Direction is set as positive x axis */
-    Grasp.pre_grasp_approach.direction.vector.x = 1.0;
-    Grasp.pre_grasp_approach.min_distance = 0.095;
-    Grasp.pre_grasp_approach.desired_distance = 0.115;
-
-    #/* Defined with respect to frame_id */
-    Grasp.post_grasp_retreat.direction.header.frame_id = "base_link";
-    #/* Direction is set as positive z axis */
-    Grasp.post_grasp_retreat.direction.vector.z = 1.0;
-    Grasp.post_grasp_retreat.min_distance = 0.1;
-    Grasp.post_grasp_retreat.desired_distance = 0.25;
-
-    move_group.setSupportSurfaceName("table")
-    move_group.pick("object", Grasp);
-#moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-#moveit::planning_interface::MoveGroupInterface group("panda_arm");
-
-if __name__ == '__main__':
-    rospy.init_node("demo")
-
-    # Create move group interface for a fetch robot
-    move_group = MoveGroupInterface("arm_with_torso","base_link")
-
-    # Define ground plane
-    # This creates objects in the     planning scene that mimic the ground
-    # If these were not in place gripper could hit the ground
-    planning_scene = PlanningSceneInterface()
+        # clean the scene
+        scene.remove_world_object("table")
+        scene.remove_world_object("part")
     
-    while not rospy.is_shutdown():
-        addCollisionObjects(planning_scene)
-        pick(move_group)
+        right_arm.set_named_target("resting")
+        right_arm.go()
+       
+        right_gripper.set_named_target("open")
+        right_gripper.go()
+       
+        rospy.sleep(1)
+    
+        # publish a demo scene
+        p = PoseStamped()
+        p.header.frame_id = robot.get_planning_frame()
+    
+        # add a table
+        #p.pose.position.x = 0.42
+        #p.pose.position.y = -0.2
+        #p.pose.position.z = 0.3
+        #scene.add_box("table", p, (0.5, 1.5, 0.6))
+
+    
+        # add an object to be grasped
+        p.pose.position.x = 0.15
+
+        p.pose.position.y = -0.12
+        p.pose.position.z = 0.7
+        scene.add_box("part", p, (0.07, 0.01, 0.2))
+       
+        rospy.sleep(1)
+              
+        g = Grasp()
+        g.id = "test"
+        start_pose = PoseStamped()
+        start_pose.header.frame_id = FIXED_FRAME
+    
+        # start the gripper in a neutral pose part way to the target
+        start_pose.pose.position.x = 0.0130178
+        start_pose.pose.position.y = -0.125155
+        start_pose.pose.position.z = 0.597653
+        start_pose.pose.orientation.x = 0.0
+        start_pose.pose.orientation.y = 0.388109
+        start_pose.pose.orientation.z = 0.0
+        start_pose.pose.orientation.w = 0.921613
+           
+        right_arm.set_pose_target(start_pose)
+        right_arm.go()
+        
+        rospy.sleep(2)
+
+        # generate a list of grasps
+        grasps = self.make_grasps(start_pose)
+    
+        result = False
+        n_attempts = 0
+        
+        # repeat until will succeed
+        while result == False:
+            result = robot.right_arm.pick("part", grasps)       
+            n_attempts += 1
+            print "Attempts: ", n_attempts
+            rospy.sleep(0.2)
+           
+        rospy.spin()
+        roscpp_shutdown()
+        
+        
+    # Get the gripper posture as a JointTrajectory
+    def make_gripper_posture(self, pose):
+        t = JointTrajectory()
+        t.joint_names = GRIPPER_JOINT_NAMES
+        tp = JointTrajectoryPoint()
+        tp.positions = [pose for j in t.joint_names]
+        tp.effort = GRIPPER_EFFORT
+        t.points.append(tp)
+        return t
+    
+    def make_gripper_translation(self, min_dist, desired, axis=1.0):
+        g = GripperTranslation()
+        g.direction.vector.x = axis
+        g.direction.header.frame_id = GRIPPER_FRAME
+        g.min_distance = min_dist
+        g.desired_distance = desired
+        return g
+
+    def make_grasps(self, pose_stamped, mega_angle=False):
+        # setup defaults for the grasp
+        g = Grasp()
+        g.pre_grasp_posture = self.make_gripper_posture(GRIPPER_OPEN)
+        g.grasp_posture = self.make_gripper_posture(GRIPPER_CLOSED)
+        g.pre_grasp_approach = self.make_gripper_translation(0.05, 0.1)
+        g.post_grasp_retreat = self.make_gripper_translation(0.05, 0.1, -1.0)
+        g.grasp_pose = pose_stamped
+    
+        pitch_vals = [0, 0.2, -0.2, 0.4, -0.4]
+        #pitch_vals = [0]
+        
+        yaw_vals = [-0.2, -0.1, 0, 0.1, 0.2]
+        #yaw_vals = [0]
+        
+        if mega_angle:
+            pitch_vals += [0.78, -0.78, 0.3, -0.3, 0.5, -0.5, 0.6, -0.6]
+    
+        # generate list of grasps
+        grasps = []
+        #for y in [-1.57, -0.78, 0, 0.78, 1.57]:
+        for y in yaw_vals:
+            for p in pitch_vals:
+                q = quaternion_from_euler(0, 1.57-p, y)
+                g.grasp_pose.pose.orientation.x = q[0]
+                g.grasp_pose.pose.orientation.y = q[1]
+                g.grasp_pose.pose.orientation.z = q[2]
+                g.grasp_pose.pose.orientation.w = q[3]
+                g.id = str(len(grasps))
+                g.allowed_touch_objects = ["part"]
+                g.max_contact_force = 0
+                #g.grasp_quality = 1.0 - abs(p/2.0)
+                grasps.append(copy.deepcopy(g))
+        return grasps
+
+
+if __name__=='__main__':
+    TestPick()
